0e9959e4ef1a3d80a7d7f99cf0c4c145f159f61c
Aleksandar Milinkovic <milinkovicaleks@gmail.com>
2024-01-29 14:02:52 +0100

    added console output test - unstable

---
+++ tests/ui/console/test_console.py
@@ -0,0 +1,21 @@
+import unittest
+import io
+import sys
+
+from src.ui.console import print_board
+
+class TestConsole(unittest.TestCase):
+     def test_print_board(self):
+        board = [['X', 'O', 'X'], ['O', 'X', 'O'], [' ', ' ', 'X']]
+
+        expected_output = "X|O|X\n-----\nO|X|O\n-----\n | |X\n"
+
+        captured_output = io.StringIO()
+        sys.stdout = captured_output
+
+        print_board(board)
+
+        # Reset stdout
+        sys.stdout = sys.__stdout__
+
+        self.assertEqual(captured_output.getvalue(), expected_output)
